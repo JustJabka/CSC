66d123b511b3d6c8fa027ecabc94c527d4230f07
JustJabka <ivanmartinisin49@gmail.com>
2026-05-28 12:50:48 +0300

    Added ability bar

--- src/main/java/justjabka/csc/rendering/CSCAbilityRendering.java
+++ src/main/java/justjabka/csc/rendering/CSCAbilityRendering.java
@@ -1,41 +1,67 @@
 package justjabka.csc.rendering;
 
-import justjabka.csc.contents.ability.generic.ActiveAbility;
-import justjabka.csc.handlers.AbilityHandler;
+import justjabka.csc.CSC;
+import justjabka.csc.contents.attachement.PlayerData;
 import justjabka.csc.registries.CSCAttachments;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Player;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CSCAbilityRendering {
+    private static final Identifier ABILITY_BAR_BACKGROUND = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/ability_bar_background");
+    private static final Identifier ABILITY_BAR_PROGRESS = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/ability_bar_progress");
+
     public static void render(GuiGraphics graphics, Font font, Player player, int sw, int sh) {
-        AbilityHandler handler = player.getAttached(CSCAttachments.ABILITY_HANDLER);
-        if (handler == null) return;
+        PlayerData data = player.getAttached(CSCAttachments.PLAYER_DATA);
+        if (data == null) return;
 
+        AtomicInteger yOffset = new AtomicInteger(0);
+        int yOffsetStep = 16;
 
-        for (ActiveAbility ability : handler.getActiveAbilities()) {
-            int remainingSeconds = ability.getRemainingSeconds();
+        data.abilities().forEach((key, duration) -> {
+            int remainingSeconds = duration / 20;
+            renderAbility(graphics, font, player, sw, sh, remainingSeconds, yOffset.get());
 
-            renderAbilities(graphics, font, player, sw, sh, remainingSeconds);
-        }
+            yOffset.addAndGet(yOffsetStep);
+        });
     }
 
-    public static void renderAbilities(
+    public static void renderAbility(
             GuiGraphics graphics,
             Font font,
             Player player,
             int sw,
             int sh,
-            int remainingSeconds
+            int remainingSeconds,
+            int yOffset
     ) {
+        int baseX = 0;
+        int baseY = 52 + yOffset;
+
         Component text = Component
                 .literal(String.valueOf(remainingSeconds))
                 .withStyle(style -> style
                         .withColor(ChatFormatting.WHITE)
                 );
 
-        graphics.drawCenteredString(font, text, sw / 2, sh / 2, 0xFFFFFFFF);
+        CSCHudRendering.renderBar(
+                graphics,
+                font,
+                ABILITY_BAR_BACKGROUND,
+                ABILITY_BAR_PROGRESS,
+                1,
+                text,
+                sw,
+                sh,
+                baseX,
+                baseY,
+                86,
+                12
+        );
     }
 }
