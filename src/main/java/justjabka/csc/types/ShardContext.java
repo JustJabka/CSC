baf55073d401bbf2f6b7d724f044099eefaa5427
JustJabka <ivanmartinisin49@gmail.com>
2026-06-10 21:55:22 +0300

    Added shard context

--- src/main/java/justjabka/csc/types/ShardContext.java
+++ src/main/java/justjabka/csc/types/ShardContext.java
@@ -1,4 +1,18 @@
 package justjabka.csc.types;
 
+import justjabka.csc.contents.character.generic.BaseCharacter;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import org.jspecify.annotations.Nullable;
+
 public class ShardContext {
+    public final Player player;
+    public final ItemStack shardStack;
+    public final @Nullable BaseCharacter character;
+
+    public ShardContext(Player player, ItemStack shardStack, @Nullable BaseCharacter character) {
+        this.player = player;
+        this.shardStack = shardStack;
+        this.character = character;
+    }
 }
