be3af331ab443acaf10db9f226c686df43315cd5
JustJabka <ivanmartinisin49@gmail.com>
2026-06-12 23:30:23 +0300

    Added absorption display for health bar

--- src/main/java/justjabka/csc/rendering/CSCHealthRendering.java
+++ src/main/java/justjabka/csc/rendering/CSCHealthRendering.java
@@ -8,24 +8,29 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.FontDescription;
 import net.minecraft.resources.Identifier;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
 
 public class CSCHealthRendering {
-//    private static final Identifier ICON = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/health_bar_icon");
     private static final Identifier ICON_FONT = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "icons");
     private static final Identifier HEALTH_BAR_BACKGROUND = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/health_bar_background");
     private static final Identifier HEALTH_BAR_PROGRESS = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/health_bar_progress");
+    private static final Identifier HEALTH_BAR_ABSORPTION_PROGRESS = Identifier.fromNamespaceAndPath(CSC.MOD_ID, "hud/health_bar_absorption_progress");
 
     public static void render(GuiGraphicsExtractor graphics, Font font, Player player, int sw, int sh) {
         int currentHealth = Math.round(player.getHealth());
         int maxHealth = Math.round(player.getMaxHealth());
         float healthPercent = currentHealth / (float) maxHealth;
 
-//        Component icon = Component
-//                .object(new AtlasSprite(AtlasIds.GUI, ICON))
-//                .withStyle(style -> style
-//                        .withColor(ChatFormatting.WHITE)
-//                        .withoutShadow()
-//                );
+        int currentAbsorptionAmount = Math.round(player.getAbsorptionAmount());
+        boolean hasAbsorption = currentAbsorptionAmount > 0;
+
+        currentHealth = hasAbsorption ? currentHealth + currentAbsorptionAmount : currentHealth;
+        Identifier barProgress = hasAbsorption ? HEALTH_BAR_ABSORPTION_PROGRESS : HEALTH_BAR_PROGRESS;
+
+        renderBar(graphics, font, player, sw, sh, currentHealth, maxHealth, healthPercent, barProgress);
+    }
+
+    private static void renderBar(GuiGraphicsExtractor graphics, Font font, Player player, int sw, int sh, int currentHealth, int maxHealth, float healthPercent, Identifier barProgress) {
         Component icon = Component
                 .literal("❤")
                 .withStyle(style -> style
@@ -44,21 +49,28 @@ public class CSCHealthRendering {
                 .append(icon)
                 .append(info);
 
-        if (player.gameMode().isSurvival()) {
-            CSCHudRendering.renderBar(
-                    graphics,
-                    font,
-                    HEALTH_BAR_BACKGROUND,
-                    HEALTH_BAR_PROGRESS,
-                    healthPercent,
-                    text,
-                    sw,
-                    sh,
-                    0,
-                    38,
-                    182,
-                    12
-            );
-        }
+        if (!showHealthBar(player)) return;
+
+        CSCHudRendering.renderBar(
+                graphics,
+                font,
+                HEALTH_BAR_BACKGROUND,
+                barProgress,
+                healthPercent,
+                text,
+                sw,
+                sh,
+                0,
+                38,
+                182,
+                12
+        );
+    }
+
+    private static boolean showHealthBar(Player player) {
+        GameType gameMode = player.gameMode();
+
+        if (gameMode == null) return false;
+        return gameMode.isSurvival();
     }
 }
